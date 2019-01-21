c0d35cffd2dda90748114c5a00375fef9b000cb2
Owen Selles <owensellesyt@gmail.com>
2019-01-21 14:10:56 +0100

    Fix contact form redirect time

--- send_form_email.php
+++ send_form_email.php
@@ -1,12 +1,10 @@
 <?php
 if(isset($_POST['email'])) {
 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "me@owenselles.com";
     $email_subject = "Contact form owenselles.com";
 
     function died($error) {
-        // your error code can go here
         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
         echo "These errors appear below.<br /><br />";
         echo $error."<br /><br />";
@@ -14,19 +12,15 @@ if(isset($_POST['email'])) {
         die();
     }
 
-
-    // validation expected data exists
     if(!isset($_POST['name']) ||
         !isset($_POST['email']) ||
         !isset($_POST['message'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');
     }
 
-
-
-    $first_name = $_POST['name']; // required
-    $email_from = $_POST['email']; // required
-    $comments = $_POST['message']; // required
+    $first_name = $_POST['name'];
+    $email_from = $_POST['email'];
+    $comments = $_POST['message'];
 
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -51,48 +45,34 @@ if(isset($_POST['email'])) {
 
     $email_message = "Form details below.\n\n";
 
-
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
     }
 
-
-
     $email_message .= "Name: ".clean_string($first_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
     $email_message .= "Message: ".clean_string($comments)."\n";
 
-// create email headers
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
 ?>
 
-<!-- include your own success html here -->
 <html>
    <head>
-
       <script type="text/javascript">
-         <!--
             function Redirect() {
                window.location="https://www.owenselles.com/";
             }
-
-            document.write("Thanks for contacting me. I will reply soon.");
-            document.write("<br><br>You will be redirected to main page in a few seconds.");
-            setTimeout('Redirect()', 4000);
-         //-->
+            document.write("Done");
+            setTimeout('Redirect()', 750);
       </script>
-
    </head>
-
    <body>
    </body>
 </html>
-
-
 <?php
 
 }
